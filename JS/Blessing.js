49687dc1e0a7452aa4a4cd6aaad6ce85619ad31b
raxtorgaming <152212848+raxtorgaming@users.noreply.github.com>
2024-02-14 03:18:57 +0700

    Update Blessing.js

--- JS/Blessing.js
+++ JS/Blessing.js
@@ -1,37 +1,23 @@
-var song = document.getElementById("audio");
-song.volume = 0.1;
-song.currentTime = 67.5;
-
-setTimeout(function(){
-    song.play();
-
-    setTimeout(function(){
-      song.pause();
-      song.currentTime = 67.5;
-    }, 2000);
-}, 14200);
-
+var lastScrollTop = 0;
 
 $(window).scroll(function () {
-  var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-  $(".fade").each(function () {
-    /* Check the location of each desired element */
-    var objectBottom = $(this).offset().top + $(this).outerHeight();
+  var windowTop = $(this).scrollTop();
+  
+  if (windowTop > lastScrollTop) { // Jika menggulir ke bawah
+    var windowBottom = windowTop + $(window).height();
+    $(".fade").each(function () {
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      if (objectBottom < windowBottom) { // Jika elemen berada dalam jendela saat menggulir ke bawah
+        if ($(this).css("opacity") == 0) {
+          $(this).css( "30%"); // Set radius di sini
+          $(this).fadeTo(80, 1);
+        }
+      }
+    });
+  }
 
-    /* If the element is completely within bounds of the window, fade it in */
-    if (objectBottom < windowBottom) { // (scrolling bawah)
-      if ($(this).css("opacity") == 0) {
-        $(this).css( "30%"); // Set radius di sini
-        $(this).fadeTo(80, 1);
-      }
-    } else { // (scroll atas)
-      if ($(this).css("opacity") == 1) {
-        $(this).fadeTo(80, 1);
-      }
-    }
-  });
+  lastScrollTop = windowTop;
 }).scroll(); //invoke scroll-handler on page-load
-
 
 $("button").click(function(){
   window.location.replace("mawar.html");
