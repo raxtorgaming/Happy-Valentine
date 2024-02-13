67ba76d3b01abc7714b1b48152415e0bf3f93372
raxtorgaming <152212848+raxtorgaming@users.noreply.github.com>
2024-02-14 03:28:18 +0700

    Update Blessing.js

--- JS/Blessing.js
+++ JS/Blessing.js
@@ -1,23 +1,36 @@
-var lastScrollTop = 0;
+var song = document.getElementById("audio");
+song.volume = 0.1;
+song.currentTime = 67.5;
 
-$(window).scroll(function () {
-  var windowTop = $(this).scrollTop();
-  
-  if (windowTop > lastScrollTop) { // Jika menggulir ke bawah
-    var windowBottom = windowTop + $(window).height();
+setTimeout(function(){
+    song.play();
+
+    setTimeout(function(){
+      song.pause();
+      song.currentTime = 67.5;
+    }, 2000);
+}, 14200);
+
+
+$(window).on("load", function () {
+  $(window).scroll(function () {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
     $(".fade").each(function () {
+      /* Check the location of each desired element */
       var objectBottom = $(this).offset().top + $(this).outerHeight();
-      if (objectBottom < windowBottom) { // Jika elemen berada dalam jendela saat menggulir ke bawah
+
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { // (scrolling bawah)
         if ($(this).css("opacity") == 0) {
-          $(this).css( "30%"); // Set radius di sini
-          $(this).fadeTo(80, 1);
+          $(this).fadeTo(200, 1);
         }
+      
+        
       }
     });
-  }
+  }).scroll(); //invoke scroll-handler on page-load
+});
 
-  lastScrollTop = windowTop;
-}).scroll(); //invoke scroll-handler on page-load
 
 $("button").click(function(){
   window.location.replace("mawar.html");
