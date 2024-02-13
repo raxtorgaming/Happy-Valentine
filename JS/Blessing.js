53aaa2153d0ffe71cf456361262a4401aa57ddb6
raxtorgaming <152212848+raxtorgaming@users.noreply.github.com>
2024-02-14 03:03:26 +0700

    Update Blessing.js

--- JS/Blessing.js
+++ JS/Blessing.js
@@ -12,41 +12,25 @@ setTimeout(function(){
 }, 14200);
 
 
-$(window).on("load", function () {
-  var previousScrollTop = 0; // variable to store the previous scroll position
-  $(window).scroll(function () {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".fade").each(function () {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
+$(window).scroll(function () {
+  var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+  $(".fade").each(function () {
+    /* Check the location of each desired element */
+    var objectBottom = $(this).offset().top + $(this).outerHeight();
 
-      /* If the element is completely within bounds of the window, fade it in or out based on scroll direction */
-      if (objectBottom < windowBottom) { // (scrolling bawah)
-        if ($(this).css("opacity") == 0) {
-          $(this).fadeTo(200, 1);
-        }
-      } else { // (scroll atas)
-        if ($(this).css("opacity") == 1) {
-          $(this).fadeTo(200, 0);
-        }
+    /* If the element is completely within bounds of the window, fade it in */
+    if (objectBottom < windowBottom) { // (scrolling bawah)
+      if ($(this).css("opacity") == 0) {
+        $(this).css( "30%"); // Set radius di sini
+        $(this).fadeTo(200, 1);
       }
-    });
-
-    // check if the user is scrolling up or down
-    var currentScrollTop = $(this).scrollTop();
-    if (currentScrollTop > previousScrollTop) { // scrolling down
-      // do something for scrolling down
-    } else { // scrolling up
-      // do something for scrolling up
-      $(".fade").each(function () {
-        if ($(this).css("opacity") == 1) {
-          $(this).fadeTo(200, 0);
-        }
-      });
+    } else { // (scroll atas)
+      if ($(this).css("opacity") == 1) {
+        $(this).fadeTo(200, 0);
+      }
     }
-    previousScrollTop = currentScrollTop; // update the previous scroll position
-  }).scroll(); //invoke scroll-handler on page-load
-});
+  });
+}).scroll(); //invoke scroll-handler on page-load
 
 
 $("button").click(function(){
